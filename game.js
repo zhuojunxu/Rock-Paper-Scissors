0e52e61d3ea41120df590d0e0fb4eb839dbde10e
zhuojunxu <xuzhuojun1128@126.com>
2023-05-18 15:21:41 -0400

    Logic for the game have completed, including restarting game, next step is add UI.

--- game.js
+++ game.js
@@ -13,17 +13,25 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     // your code here!
     if (playerSelection === computerSelection) {
-        return "Game tie";
+        document.getElementById('state2').innerHTML = " Game Tie"; 
     } else {
         if ((playerSelection === "Rock" && computerSelection === "Scissors") || (playerSelection === "Scissors" && computerSelection === "Paper") || (playerSelection === "Paper" && computerSelection === "Rock")) {
             playerCounter++;
-            console.log("Player Scores:" +computerCounter);
-            return "You Win! " + playerSelection + " beats " + computerSelection;
+            document.getElementById('player').innerHTML = "Player Score: " + playerCounter;
+            document.getElementById('state2').innerHTML = " You Win! " + playerSelection + " beats " + computerSelection;
         } else if ((playerSelection === "Scissors" && computerSelection === "Rock") || (playerSelection === "Paper" && computerSelection === "Scissors") || (playerSelection === "Rock" && computerSelection === "Paper")) {
             computerCounter++;
-            console.log("Computer Scores:" +computerCounter);
-            return "You Lose! " + computerSelection + " beats " + playerSelection;
+            document.getElementById('comp').innerHTML = "Computer Score: " + computerCounter;
+            document.getElementById('state2').innerHTML = " You Lose! " + computerSelection + " beats " + playerSelection;
         }
+        if (playerCounter == 5) {
+            document.getElementById('state2').innerHTML = "You Win! Please Click Reset Button Below."
+            endGame();
+        } else if (computerCounter == 5) {
+            document.getElementById('state2').innerHTML = "Computer Wins! Please Click Reset Button Below."
+            endGame();
+        }
+
     }
 }
 
@@ -31,30 +39,56 @@ function playRound(playerSelection, computerSelection) {
 // while 1 means game coming on till either computer/player reach 5 rounds first.
 // change from while(1) to for (let i = 0; i <= 4; i++) means the total number of rounds will be 5. 
 function game() {
-    while (1) {
-        const playerSelectionInit = prompt("Please Enter the result");
-        let playerSelection = playerSelectionInit.charAt(0).toUpperCase() + playerSelectionInit.substring(1).toLowerCase();
-        const computerSelection = getComputerChoice();
-        console.log(playRound(playerSelection, computerSelection));
-        if (playerCounter == 5 || computerCounter == 5) {
-            break;
-        }
-    }
+
+    document.getElementById("Reset").style.visibility = "hidden";
+
+    const paperPerson = document.getElementById('Paper');
+    paperContent = paperPerson.textContent;
+
+    paperPerson.addEventListener("click", (paperPerson) => playRound(paperContent, getComputerChoice()));
+
+    const rockPerson = document.getElementById('Rock');
+    rockContent = rockPerson.textContent;
+
+    rockPerson.addEventListener("click", (rockPerson) => playRound(rockContent, getComputerChoice()));
+
+
+    const scissorsPerson = document.getElementById('Scissors');
+    scissorsContent = scissorsPerson.textContent;
+
+    scissorsPerson.addEventListener("click", (scissorsPerson) => playRound(scissorsContent, getComputerChoice()));
+
 }
 
-function result() {
-    if (computerCounter == playerCounter) {
-        console.log("Overall Game Result are tied.")
-    } else if (computerCounter < playerCounter) {
-        console.log("You win to the computer.");
-    } else {
-        console.log("You lose to the computer.")
-    }
+
+function paperFunction(paper2) {
+    paper.playRound(paper2, getComputerChoice());
 }
 
-const gameProcess = game();
-console.log(gameProcess);
-const gameResult = result();
-console.log(gameResult);
-console.log("Computer scores: " + computerCounter);
-console.log("Player scores: " + playerCounter);
+
+function endGame() {
+    document.getElementById("Paper").disabled = true;
+    document.getElementById("Rock").disabled = true;
+    document.getElementById("Scissors").disabled = true;
+
+    let element = document.getElementById("Reset");
+    let notHidden = element.style.visibility = "visible";
+
+    element.addEventListener("click", (element) => resetGame());
+}
+
+
+function resetGame(element) {
+    document.getElementById("Reset").style.visibility = "hidden";
+    playerCounter = 0;
+    computerCounter = 0;
+    document.getElementById('player').innerHTML = "Player Score: " + playerCounter;
+    document.getElementById('comp').innerHTML = "Computer Score: " + computerCounter;
+    document.getElementById('state2').innerHTML = "";
+    document.getElementById('state3').innerHTML = "";
+    document.getElementById("Paper").disabled = false;
+    document.getElementById("Rock").disabled = false;
+    document.getElementById("Scissors").disabled = false;
+}
+
+game();
